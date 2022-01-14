b2b729a4d34e776b6afae72ec99198895def4831
novaardiansyah <novaardiansyah78@gmail.com>
2022-01-14 07:03:00 +0000

    set up card components
    set up a progress bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Container, Stack, Button } from 'react-bootstrap'
+import BudgetCard from './components/BudgetCard'
+
 function App() {
-  return "Hello World"
+  return (
+    <Container className='my-4'>
+      <Stack direction='horizontal' gap='2' className='mb-4'>
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='outline-primary'>Add Expense</Button>
+      </Stack>
+
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+          gridGap: '1rem',
+          alignItems: 'flex-start',
+        }}
+      >
+        <BudgetCard name="Entertainment" amount="300" max="1000" />
+      </div>
+    </Container>
+  )
 }
 
-export default App;
+export default App
